76e6f141937aaf7323698e26daba4802e33139b4
AustinFSSE <108036029+AustinFSSE@users.noreply.github.com>
2025-04-06 21:56:33 -0400

    commit from windows

--- 03-queues-and-hashing/queueADT.cpp
+++ 03-queues-and-hashing/queueADT.cpp
@@ -43,6 +43,7 @@ template<class Type>
 bool queueType<Type>::isFullQueue() const {
     return count == maxQueueSize;
 }
+
 template<class Type>
 void queueType<Type>::initializeQueue() {
     count = 0;
@@ -50,16 +51,19 @@ void queueType<Type>::initializeQueue() {
     queueRear = maxQueueSize - 1;
     list = new Type[maxQueueSize];
 }
+
 template<class Type>
 Type queueType<Type>::front() const {
     assert(count != 0);
     return list[queueFront];
 }
+
 template<class Type>
 Type queueType<Type>::back() const {
     assert(count != 0);
     return list[queueRear];
 }
+
 template<class Type>
 void queueType<Type>::addQueue(const Type &item) {
     if (!isFullQueue()) {
@@ -71,17 +75,17 @@ void queueType<Type>::addQueue(const Type &item) {
         std::cout << "Queue is full" << std::endl;
     }
 }
+
 template<class Type>
 void queueType<Type> :: deleteQueue() {
-    assert(count != 0);
     if (!isEmptyQueue()) {
-
         count--;
         queueFront = (queueFront + 1) % maxQueueSize;
     } else {
         std::cout << "Queue is empty." << std::endl;
     }
 }
+
 template<class Type>
 queueType<Type>::queueType(int capacity) {
     if (capacity <= 0) {
@@ -96,17 +100,22 @@ queueType<Type>::queueType(int capacity) {
     count = 0;
     list = new Type[maxQueueSize];
 }
+
 template<class Type>
 queueType<Type>::~queueType() {
     delete[] list;
 }
+
 template<class Type>
 void queueType<Type>::display() const {
     assert(count != 0);
     for (int i = 0; i < count; i++) {
-        std::cout << list[i] << " ";
+        int index = (queueFront + i) % maxQueueSize;
+        std::cout << list[index] << " ";
     }
+    std::cout << std::endl;
 }
+
 template<class Type>
 queueType<Type> &queueType<Type>::operator=(const queueType<Type>& otherType) {
     // check for self-assignment
@@ -134,11 +143,26 @@ queueType<Type> &queueType<Type>::operator=(const queueType<Type>& otherType) {
 
 
 int main() {
+
+    // set up the queues
     queueType<int> q1(5);
+    queueType<int> q2(5);
     q1.addQueue(10);
     q1.addQueue(20);
     q1.addQueue(30);
     q1.addQueue(40);
     q1.addQueue(50);
-    q1.display();
+    // q1.display();
+
+    std::cout << q1.front() << std::endl;
+    std::cout << q1.back() << std::endl;
+    //q1.display();
+
+
+    q1.deleteQueue();
+    //q1.display();
+
+    // display the operator effect
+    q2 = q1;
+    // q2.display();
 }
