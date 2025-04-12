2558d9f7d803e53b8b97397be29e2dc3df5ceb6e
Austin Martin <alm2016.in@icloud.com>
2025-04-12 18:41:11 -0400

    commit from mac

--- 04-programming-assignments/UniqueSets.cpp
+++ 04-programming-assignments/UniqueSets.cpp
@@ -1,12 +1,40 @@
 //
 // Created by Austin Martin on 4/12/25.
 //
-
-#include "UniqueSets.h"
 #include <iostream>
+#include <fstream>
+#include <set>
+#include <sstream>
 
 using namespace std;
 
 int main() {
-    cout << "UniqueSets" << endl;
+
+    set<string> sets;
+
+    string text = "Humpty Dumpty sat on a wall,\n"
+                  "Humpty Dumpty had a great fall All the king’s horses\n"
+                  "and all the king’s men\n"
+                  "Couldn’t put Humpty together again.\n";
+    ofstream File("Song.txt");
+
+    File << text;
+
+    File.close();
+
+    ifstream reader("Song.txt");
+    string line;
+    while (getline(reader, line)) {
+        istringstream iss(line);
+        string word;
+        while (iss >> word) {
+            sets.insert(word);
+        }
+
+    }
+
+    for (const auto& word : sets) {
+        cout << word << endl;
+    }
+    return 0;
 }
