abef29b9645218c5458124fd71e1cd302c84cc97
Austin Martin <alm2016.in@icloud.com>
2025-04-12 17:43:56 -0400

    commit from windows

---
+++ 04-programming-assignments/UniqueSets.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Austin Martin on 4/12/25.
+//
+
+#include "UniqueSets.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "UniqueSets" << endl;
+}
---
+++ 04-programming-assignments/UniqueSets.h
@@ -0,0 +1,25 @@
+//
+// Created by Austin Martin on 4/12/25.
+//
+
+#pragma once
+#include <set>
+#include <string>
+
+
+class UniqueSets {
+
+    private:
+        std::set<std::string>;
+
+    public:
+    UniqueSets() {}
+
+    void insert(std::string s) {
+
+    }
+
+
+
+};
+
