09b2a1032054546aa382e1005b0890f870f98792
Austin Martin <alm2016.in@icloud.com>
2025-04-18 16:26:52 -0400

    commit from macos

--- 05- programming-assignments/recursiveFlipper.cpp
+++ 05- programming-assignments/recursiveFlipper.cpp
@@ -7,16 +7,16 @@
 
 using namespace std;
 
-void flipper(int low, int high, int *arr) {
-  for (const auto& c : arr) {
-    cout << c << endl;
+void flipper(int low, int high, int *arr, int size) {
+  for (int i = 0; i < size; i++) {
+    cout << arr[i] << endl;
   }
 }
 
 int main() {
   int low = 0, high = 10;
   int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-  flipper(low, high, arr);
+  flipper(low, high, arr, 10);
 
   cout << "Hello World!" << endl;
 
