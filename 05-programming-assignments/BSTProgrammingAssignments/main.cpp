38f831ab0bd09497403b7e45e2136e087108fafd
AustinFSSE <108036029+AustinFSSE@users.noreply.github.com>
2025-04-18 19:31:52 -0400

    commit from windows vscode

--- 05-programming-assignments/BSTProgrammingAssignments/main.cpp
+++ 05-programming-assignments/BSTProgrammingAssignments/main.cpp
@@ -9,6 +9,14 @@ using namespace std;
 
 int main() {
     int TreeKeys[16] = {50, 76, 21, 4, 32, 64, 15, 52, 14, 100, 83, 2, 3, 70, 87, 80};
+
+    BSTNode myTree;
+
+    cout << "Printing the tree in order\nBefor adding numbers\n";
+
+    myTree.PrintInOrder();
+
+
     cout << "Hello World!" << endl;
     return 0;
 }
