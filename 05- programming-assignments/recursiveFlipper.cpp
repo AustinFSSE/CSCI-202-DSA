46af2e5544aeb7ced76f93177bf2fc04cd6995d5
Austin Martin <alm2016.in@icloud.com>
2025-04-18 15:49:58 -0400

    commit form MacOS

--- 05- programming-assignments/recursiveFlipper.cpp
+++ 05- programming-assignments/recursiveFlipper.cpp
@@ -2,10 +2,23 @@
 // Created by Austin Martin on 4/18/25.
 //
 
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+void flipper(int low, int high, int* arr) {
+  for (const int& c : arr) {
+    cout << c << "\n";
+  }
+}
 
 int main() {
+  int low = 0, high = 10;
+  int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  flipper(low, high, arr);
 
+  cout << "Hello World!" << endl;
 
   return 0;
 }
