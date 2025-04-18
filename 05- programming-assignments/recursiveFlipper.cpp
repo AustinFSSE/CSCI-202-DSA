5caa9fdabcc86abb57f1464a379a01c85401ae47
Austin Martin <alm2016.in@icloud.com>
2025-04-18 17:19:21 -0400

    programming assignment #2 finished

--- 05- programming-assignments/recursiveFlipper.cpp
+++ 05- programming-assignments/recursiveFlipper.cpp
@@ -11,16 +11,25 @@ using namespace std;
 int main() {
   int low = 3, high = 7;
   int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-  low = arr[low];
-  high = arr[high];
-  int distance = (high - low ) / 2;
-  for (int i = low - 1; i < distance; i++) {
-    int temp = arr[low];
-    arr[low] = arr[high];
-    arr[high] = arr[low];
-  }
+
+  int start = low - 1;
+  int end = high - 1;
+
   for (int i = 0; i < 10; i++) {
-    cout << arr[i] << endl;
+    cout << arr[i] << " ";
+  }
+  
+  while (start < end) {
+    int temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+    start++;
+    end--;
+  }
+  cout << endl;
+
+  for (int i = 0; i < 10; i++) {
+    cout << arr[i] << " ";
   }
   return 0;
 }
