e9bfe5dd07ed210956c0cd294c95477f316ee441
Austin Martin <alm2016.in@icloud.com>
2025-04-18 17:01:19 -0400

    commit from macos vscode

--- 05- programming-assignments/recursiveFlipper.cpp
+++ 05- programming-assignments/recursiveFlipper.cpp
@@ -8,7 +8,9 @@
 
 using namespace std;
 
-void flipper(int low, int high, int *arr, int size) {
+int main() {
+  int low = 3, high = 7;
+  int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
   low = arr[low];
   high = arr[high];
   int distance = (high - low ) / 2;
@@ -17,22 +19,8 @@ void flipper(int low, int high, int *arr, int size) {
     arr[low] = arr[high];
     arr[high] = arr[low];
   }
-}
-void displayArray(int *arr) {
-  for (int i = 0; i < arr.size; i++) {
+  for (int i = 0; i < 10; i++) {
     cout << arr[i] << endl;
   }
-}
-
-int main() {
-  int low = 3, high = 7;
-  int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-  displayArray(arr);
-  flipper(low, high, arr, 10);
-  displayArray(arr);
-
-
-  cout << "Hello World!" << endl;
-
   return 0;
 }
