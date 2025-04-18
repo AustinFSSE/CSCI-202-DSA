f5884151c9b30bf630f26b28905309e5d73f23e3
AustinFSSE <108036029+AustinFSSE@users.noreply.github.com>
2025-04-18 18:50:42 -0400

    commit from windows

---
+++ 05-programming-assignments/BSTProgrammingAssignments/BSTNode.cpp
@@ -0,0 +1,6 @@
+
+
+#include "BSTNode.h"
+
+
+
---
+++ 05-programming-assignments/BSTProgrammingAssignments/BSTNode.h
@@ -0,0 +1,16 @@
+
+
+#pragma once 
+
+class BSTNode {
+
+    private:
+        struct node 
+        {
+            int key;
+            node* left;
+            node* right;
+        };
+        node* root;
+        
+}
